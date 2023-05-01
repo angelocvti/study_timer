fac30e461f4006a079c3bfbe3321d31fefafb9da
Angelo Cavalcanti <angelo.cvti@gmail.com>
2023-05-01 20:59:59 -0300

    Adding sass; CSS reset; Creating List component.

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import Form from "../components/Form";
+import List from "../components/List";
+import "./style.scss";
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Form></Form>
+      <List></List>
+    </div>
+  );
+}
+
+export default App;
